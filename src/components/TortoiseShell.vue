8bdb7e2aec2466cd0442a8e3b3ce1b0ec06d28f0
Tymonsx <tymonsx@hotmail.com>
2020-09-09 14:02:16 -0300

    Laranja em vez de vemelho

--- src/components/TortoiseShell.vue
+++ src/components/TortoiseShell.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page :style-fn="myTweak">
     <p>
-      Tortoiseshell: Possuem trechos em sua pelagem com gradações de vermelho e
+      Tortoiseshell: Possuem trechos em sua pelagem com gradações de laranja e
       preto. O tamanho destes trechos podem variar de um salpicado até grandes
       áreas de uma única cor. Estas áreas costumam ter um formato assimétrico.
     </p>
